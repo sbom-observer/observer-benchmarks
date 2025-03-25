acf0ac5d9c5eb197d7cccb43f86df0a84a13b58a
Andreas Bielk <andreas@bielk.se>
2025-03-25 12:29:39 +0100

    adds build SBOM benchmark for Erlang OTP 27.3 (Debian)

--- cpython/benchmark.sh
+++ cpython/benchmark.sh
@@ -9,7 +9,7 @@ fi
 for os in debian; do
     OUTPUT_SBOM="cpython.observer.cdx.${os}.json"
     #CMD="observer --debug build -b /output/$OUTPUT_SBOM -- make"
-    CMD="observer --debug build -b /output/$OUTPUT_SBOM -- make && observer --debug repo --merge /output/$OUTPUT_SBOM build-observations.json"
+    CMD="observer --debug build -b /output/$OUTPUT_SBOM -- make && observer --debug repo --merge /output/$OUTPUT_SBOM build-observations.json observer.yml"
     docker run --rm -it -v debugfs:/sys/kernel/debug:rw -v .:/output --net=host --pid=host --privileged observer-benchmark-cpython-${os}:latest /bin/bash -c "$CMD"
     docker run --rm -it -v debugfs:/sys/kernel/debug:rw -v .:/output --net=host --pid=host --privileged observer-benchmark-cpython-${os}:latest /bin/bash -c "cp /src/cpython.official.spdx.json /output"
 done
